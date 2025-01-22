5a67d09b6eca6d535392338ebd72ba269b4274e8
Pedro Nunes <phna8589@gmail.com>
2025-01-21 21:57:20 -0300

    Atualização prolog

--- Tarefa II/tar2.pl
+++ Tarefa II/tar2.pl
@@ -1,2 +1,13 @@
-fat(0,1).
-fat(X,Y) :- (X > 0),X1 is X-1,fat(X1,W), Y is X*W.
+musica(mus1,ponk,s,80).
+musica(mus2,ponk,s,90).
+musica(lacrimosa,class,mozart,20).
+musica(requiem,class,mozart,40).
+musica(hungUp,apop,madona,70).
+musica(popular,apop,madona,90).
+
+gen_fav(A):- musica(Nome,A,Artista,Decada), write('Artista:'), write(Artista), write(' Nome:'), write(Nome), write(' Decada:'), write(Decada),!.
+
+dec(B):- musica(Nome,Gen,Artista,B), write('Artista:'), write(Artista), write(' Nome:'), write(Nome), write(' Genero:'), write(Gen),!.
+
+
+print(A):-forall(musica(X,A,Y,Z), writeln(X,A,Y,Z)).

5ded96002717f625fb39e9d4a4a3bc9e0561a0fa
Pedro Henrique Nunes de Assis <phna8589@gmail.com>
2025-01-29 00:48:34 -0300

    Atualização

--- Linguagens_de_Programação_Funcional/fatorial.hs
+++ Linguagens_de_Programação_Funcional/fatorial.hs
@@ -4,8 +4,8 @@ fact1(n) = n * fact1(n-1)
 fact2(n) = if n == 0 then 1 else n * fact2(n-1)
 
 fact3(n)
-|(n) == 0 = 1
-|otherwise = n * fact3(n-1)
+| (n) == 0 = 1
+| otherwise = n * fact3(n-1)
 
 
 fib(n)

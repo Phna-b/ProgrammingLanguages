63009f34c6252c7cddceb1c952362665bf67e12b
Pedro Nunes <phna8589@gmail.com>
2025-01-28 22:14:34 -0300

    atualização

--- Linguagens de Programação Funcional/fatorial.hs
+++ Linguagens de Programação Funcional/fatorial.hs
@@ -6,3 +6,11 @@ fact2(n) = if n == 0 then 1 else n * fact2(n-1)
 fact3(n)
 |(n) == 0 = 1
 |otherwise = n * fact3(n-1)
+
+
+fib(n)
+| n == 0 = 0
+| n == 1 = 1
+| otherwise = fib(n-1) + fib(n-2)
+
+par(n) = (mod n 2) == 0

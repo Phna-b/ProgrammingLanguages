74f5560a9d92cd0a5c2043c2f2c88fd8eaedf116
Pedro Nunes <phna8589@gmail.com>
2025-02-06 21:54:58 -0300

    Atualização

--- Linguagens_de_Programação_Funcional/Aula3/al3.hs
+++ Linguagens_de_Programação_Funcional/Aula3/al3.hs
@@ -1,1 +1,13 @@
-fromintegral::
+-- Retornando o penultimo 
+mysumP [a,b] = a 
+mysumP (x:xs) = mysumP xs
+
+
+-- Exemplo de entrada mysum(2 : [3])
+mysum [] = 0
+mysum (x:xs) = x + mysum xs
+
+
+
+----Fazer função que retorne o reverso da lista
+

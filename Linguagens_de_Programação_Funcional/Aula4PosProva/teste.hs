a876ac7e8e7e25f4b92ef20b4b5422f4a9ff8b90
Pedro Nunes <phna8589@gmail.com>
2025-02-20 21:37:37 -0300

    Atualização

--- Linguagens_de_Programação_Funcional/Aula4PosProva/teste.hs
+++ Linguagens_de_Programação_Funcional/Aula4PosProva/teste.hs
@@ -2,6 +2,10 @@
 aplicar _ [] = []
 aplicar f (n:ns) = (f n) : aplicar f ns
 
+--Exemplo: Retorna a raiz quadrada dos elementos da lista
+--aplicar(sqrt) [9,25,81]
+--Retorno: [3.0,5.0,9.0]
+
 
 --Recebe listas e retorna a cabeça, Recebe uma lista de lista. Função "map head" retorna as cabeças da lista
 --Aplica map head em cada uma das listas usadas como parametro  e retorna uma lista com as cabeças

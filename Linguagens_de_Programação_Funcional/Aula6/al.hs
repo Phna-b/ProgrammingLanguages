2dd5fd64f25f6792f893c8f1be8a508348dfe3c0
Pedro Nunes <phna8589@gmail.com>
2025-02-27 21:26:53 -0300

    [FEAT] Novos tipos em haskell

--- Linguagens_de_Programação_Funcional/Aula6/al.hs
+++ Linguagens_de_Programação_Funcional/Aula6/al.hs
@@ -1,6 +1,8 @@
-data Forma = Circulo Float Float Float | Retangulo Float Float Float Float
+data Forma = Circulo Float Float Float | Retangulo Float Float Float Float | Triangulo Float Float Float | Cilindro Float Float Float
 superficie::Forma -> Float
 superficie(Circulo _ _ r) = pi * r ^ 2
 superficie(Retangulo x1 y1 x2 y2) = (abs $ x2 - 1) * (abs $ y2 - y1) 
+superficie(Triangulo base altura _) = (base * altura)/2
+superficie(Cilindro raio altura _) = 2 * pi * raio
 
 --superficie $ Circulo 22.22 22.3 22.3 

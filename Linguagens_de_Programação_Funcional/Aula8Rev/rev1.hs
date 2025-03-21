c940209de0debcfd43886b43ce055940aa724101
Pedro Nunes <phna8589@gmail.com>
2025-03-20 22:10:25 -0300

    [FEAT] Revisão parte 2

--- Linguagens_de_Programação_Funcional/Aula8Rev/rev1.hs
+++ Linguagens_de_Programação_Funcional/Aula8Rev/rev1.hs
@@ -13,3 +13,19 @@ mysumP (x:xs) = mysumP xs
 pega x = mysumP(take 5 x) 
 
     
+--4
+
+data Forma = Circulo Float Float Float | Retangulo Float Float Float Float | Triangulo Float Float Float | Cilindro Float Float Float deriving (Eq, Show)
+superficie::Forma -> Float
+superficie(Retangulo x1 y1 x2 y2) = (abs $ x2 - 1) * (abs $ y2 - y1) 
+
+areaRet = do
+    putStrLn "Qual a base do retangulo?"
+    msg <- getLine
+    let msg1 = read msg :: Int 
+    putStrLn "Digite a altura do retangulo?"
+    des <- getLine
+    let altura = read des :: Int 
+    teste = altura*msg1
+    putStrLn $ "Area: "
+    

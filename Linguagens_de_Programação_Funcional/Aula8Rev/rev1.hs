bac01cb71532eb0bc870de57d80a87cafff58f7e
Pedro Nunes <phna8589@gmail.com>
2025-03-24 21:36:56 -0300

    [DOCS] Correção

--- Linguagens_de_Programação_Funcional/Aula8Rev/rev1.hs
+++ Linguagens_de_Programação_Funcional/Aula8Rev/rev1.hs
@@ -5,8 +5,8 @@ ordenar = sort
 
 --2 
 
---Uma lista só possui um único tipo em sua composição, o código possui listas e inteiros. O exemplo correto seira [(1,2),(4,5)].
-
+--Uma lista só possui um único tipo em sua composição, o código possui listas e inteiros. O exemplo correto seria [[1,2],[4,5]].
+ sort[[1,2],[4,5]]
 --3 
 mysumP [a,b] = b
 mysumP (x:xs) = mysumP xs 
